525c7984dcfd6ba10697aea8901cd1c6f5a0c2fc
Nikil O Sivakumaar <es25btech11028@iith.ac.in>
2026-03-26 01:58:41 +0530

    Add lexer dependency and implement main functionality with example tejas code

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,23 @@
+use lexer::*;
+use sourcer::*;
+use std::path::Path;
+
 fn main() {
-    println!("Hello, world!");
+    let mut sm = SourceManager::new();
+    let (_, text) = load_from_file(&mut sm, Path::new("tejas_tests/test1.tej"))
+        .expect("Error while loading 'tests/test1.tej' file.'");
+
+    let mut lexer = Lexer::new(text);
+    while !lexer.is_finished() {
+        let res = lexer
+            .scan_once()
+            .unwrap_or_else(|e| panic!("{}", e.to_string()));
+        if let Some(tok) = res {
+            println!(
+                "{} lexeme : '{}'",
+                tok,
+                text.slice_span(tok.span()).unwrap()
+            );
+        }
+    }
 }
